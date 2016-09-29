ab386d403d323f0e0dae025d7a431a37bbf54927
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2016-09-28 21:39:26 -0700

    completed base assignment1

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -7,10 +7,28 @@
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
-        $scope.lunchList = "";
+        $scope.lunchMenu = "";
         $scope.lunchMessage = "";
+        $scope.countItems = function() {
+            var itemList = $scope.lunchMenu.split(',');
+            console.log(itemList);
+            return itemList.length;
+        };
         $scope.checkIfTooMuch = function () {
-            return "Test";
+            var cleanMenu = $scope.lunchMenu.replace(/\s+/g, '');
+            console.log(cleanMenu);
+            if (cleanMenu === "") {
+                $scope.lunchMessage = "Please enter data first";
+            } else {
+                var numberOfItems = $scope.countItems();
+                console.log(numberOfItems);
+
+                if (numberOfItems <= 3) {
+                    $scope.lunchMessage = "Enjoy!";
+                } else {
+                    $scope.lunchMessage = "Too much!";
+                }
+            }
         };
     }
 
