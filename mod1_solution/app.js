7bde498b22bdcdffbe3dcb0e038c667cc0f24f02
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2016-09-28 22:06:41 -0700

    Extra credit: don't count empty items

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -9,20 +9,24 @@
     function LunchCheckController($scope) {
         $scope.lunchMenu = "";
         $scope.lunchMessage = "";
-        $scope.countItems = function() {
-            var itemList = $scope.lunchMenu.split(',');
-            console.log(itemList);
-            return itemList.length;
+        $scope.countItems = function(cleanMenu) {
+            var itemList = cleanMenu.split(',');
+            var counter = 0;
+            for (var i=0; i<itemList.length; i++) {
+                if (itemList[i] !== "") {
+                    // console.log("a real item:" + itemList[i]);
+                    counter ++;
+                }
+            }
+            return counter;
         };
         $scope.checkIfTooMuch = function () {
             var cleanMenu = $scope.lunchMenu.replace(/\s+/g, '');
-            console.log(cleanMenu);
+            // console.log("clean menu:" + cleanMenu);
             if (cleanMenu === "") {
                 $scope.lunchMessage = "Please enter data first";
             } else {
-                var numberOfItems = $scope.countItems();
-                console.log(numberOfItems);
-
+                var numberOfItems = $scope.countItems(cleanMenu);
                 if (numberOfItems <= 3) {
                     $scope.lunchMessage = "Enjoy!";
                 } else {
